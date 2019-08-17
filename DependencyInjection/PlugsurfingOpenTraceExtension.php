d8f4e9fc4c4a3747b3c2867d0cd338296489b0c3
Anton Chernikov <anton.chernikov@plugsurfing.com>
2019-08-17 17:17:50 +0300

    some fixes

--- DependencyInjection/PlugsurfingOpenTraceExtension.php
+++ DependencyInjection/PlugsurfingOpenTraceExtension.php
@@ -1,6 +1,11 @@
 <?php
 
+namespace Plugsurfing\OpenTraceBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class PlugsurfingOpenTraceExtension extends Extension
 {
--- PlugsurfingOpenTraceBundle.php
+++ PlugsurfingOpenTraceBundle.php
@@ -3,8 +3,12 @@
 namespace Plugsurfing\OpenTraceBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Plugsurfing\OpenTraceBundle\DependencyInjection\PlugsurfingOpenTraceExtension;
 
 class PlugsurfingOpenTraceBundle extends Bundle
 {
-
+    public function getContainerExtension()
+    {
+        return new PlugsurfingOpenTraceExtension();
+    }
 }
