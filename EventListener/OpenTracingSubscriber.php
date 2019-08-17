3c9c8fc406477dbbe95944dc6fcdf807df8312fc
Anton Chernikov <anton.chernikov@plugsurfing.com>
2019-08-17 15:50:59 +0300

    initial commit

---
+++ EventListener/OpenTracingSubscriber.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Plugsurfing\HelperBundle\EventListener;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class OpenTracingSubscriber implements EventSubscriberInterface
+{
+    public static function getSubscribedEvents()
+    {
+        return [KernelEvents::RESPONSE => 'onKernelResponse'];
+    }
+
+    /**
+     * Puts open trace headers from request to a response
+     * In purpose to pass them to another micro service if there is a cross service request.
+     */
+    public function onKernelResponse(FilterResponseEvent $event): void
+    {
+        $request  = $event->getRequest();
+        $response = $event->getResponse();
+
+        $openTraceHeaders = ['x-request-id', 'x-b3-traceid', 'x-b3-spanid', 'x-b3-parentspanid', 'x-b3-sampled', 'x-b3-flags', 'x-ot-span-context'];
+
+        foreach ($openTraceHeaders as $openTraceHeader) {
+            if ($request->headers->get($openTraceHeader)) {
+                $response->headers->set($openTraceHeader, $request->headers->get($openTraceHeader));
+            }
+        }
+    }
+}
---
+++ Logger/Monolog/RequestIdProcessor.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Plugsurfing\HelperBundle\Logger\Monolog;
+
+use Monolog\Processor\ProcessorInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+final class RequestIdProcessor implements ProcessorInterface
+{
+    /**
+     * @var RequestStack
+     */
+    private $requestStack;
+
+    public function __construct(RequestStack $requestStack)
+    {
+        $this->requestStack = $requestStack;
+    }
+
+    public function __invoke(array $record): array
+    {
+        if (!$this->requestStack->getCurrentRequest() instanceof Request ||
+            !$requestId = $this->requestStack->getCurrentRequest()->headers->get('x-request-id')
+        ) {
+            return $record;
+        }
+
+        $record['extra']['x-request-id'] = $requestId;
+
+        return $record;
+    }
+}
